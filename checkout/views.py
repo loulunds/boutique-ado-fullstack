f2e9efe11ef94f8374a95709139fe2b1027ce6ac
loulunds <lousanne.lundstrom@gmail.com>
2020-10-23 10:14:15 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -19,8 +19,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_keys':
-            'pk_test_51HfEr1KHHwx62Xl9rm1z4fiL4Bi5TwnHlToBgl9qVTOQmdRqhzHsu1F2eFrKt0LA4GFCgddJLuEfBnE2CcvSOMtP00nvQ8txzC',
+        'stripe_public_key': 'pk_test_51HfEr1KHHwx62Xl9rm1z4fiL4Bi5TwnHlToBgl9qVTOQmdRqhzHsu1F2eFrKt0LA4GFCgddJLuEfBnE2CcvSOMtP00nvQ8txzC',
         'client_secret': 'test client secret',
     }
 
